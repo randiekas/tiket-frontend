af0242344e4b82be18ed824eb2a294799e989ebf
randiekas <randiekas@gmail.com>
2022-08-20 10:22:13 +0700

    update nuxt config

--- nuxt.config.js
+++ nuxt.config.js
@@ -65,8 +65,8 @@ export default {
 		},
 		strategies: {
 			google: {
-				// clientId: '459074254974-e0l81r9tif92osj98ksurom0jq9v4hcc.apps.googleusercontent.com',
-				clientId: '564028017740-p7es7i3bmudj1dt47me1hhpqnmnhp2sv.apps.googleusercontent.com',
+				clientId: '459074254974-e0l81r9tif92osj98ksurom0jq9v4hcc.apps.googleusercontent.com',
+				// clientId: '564028017740-p7es7i3bmudj1dt47me1hhpqnmnhp2sv.apps.googleusercontent.com',
 				scheme: 'oauth2',
 				endpoints: {
 					authorization: 'https://accounts.google.com/o/oauth2/auth',
